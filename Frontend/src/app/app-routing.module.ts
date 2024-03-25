bde6261630381e2908195124b3c3b30e686b4107
Amanda Parreño <amanda.psierra@gmail.com>
2024-03-25 19:17:27 +0100

    app

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component'; // Asegúrate de importar el componente Home
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Esta es la ruta raíz que apunta al componente Home
+  
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
