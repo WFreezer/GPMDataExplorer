2afc14e7c41ae3f1831ebfd07d4ec476b47333e1
Amanda Parreño <amanda.psierra@gmail.com>
2024-04-02 13:57:44 +0200

    Limpiar sessiones y todas las tablas asociadas, comprobar

--- Backend/src/middlewares/cleanExpiredSessions.js
+++ Backend/src/middlewares/cleanExpiredSessions.js
@@ -2,9 +2,27 @@ const db = require('../config/dbconnector');
 
 const cleanExpiredSessions = async (req, res, next) => {
     try {
-        // Eliminar sesiones expiradas
-        const deleteQuery = 'DELETE FROM session WHERE expiration < NOW()';
-        await db.query(deleteQuery);
+        // Buscar las sesiones expiradas
+        const findExpiredSessionsQuery = 'SELECT session_id FROM session WHERE expiration < NOW()';
+        const expiredSessions = await db.query(findExpiredSessionsQuery);
+
+        if (expiredSessions.length > 0) {
+            // Obtener los IDs de las sesiones expiradas
+            const expiredSessionIds = expiredSessions.map(session => session.session_id);
+
+            // Eliminar productos asociados a las sesiones expiradas
+            const deleteProductsQuery = `DELETE FROM product WHERE session_id IN (${expiredSessionIds.map(id => `'${id}'`).join(',')})`;
+            await db.query(deleteProductsQuery);
+
+            // Eliminar filtros asociados a los productos eliminados
+            const deleteFiltersQuery = `DELETE FROM filter WHERE product_id IN (SELECT product_id FROM product WHERE session_id IN (${expiredSessionIds.map(id => `'${id}'`).join(',')}))`;
+            await db.query(deleteFiltersQuery);
+
+            // Eliminar datos meteorológicos asociados a los filtros eliminados
+            const deleteMeteorologicalDataQuery = `DELETE FROM meteorological_data WHERE id_filter IN (SELECT id_filter FROM filter WHERE product_id IN (SELECT product_id FROM product WHERE session_id IN (${expiredSessionIds.map(id => `'${id}'`).join(',')})))`;
+            await db.query(deleteMeteorologicalDataQuery);
+        }
+
         console.log("Realizando limpieza de sesiones expiradas");
         // Llama a next() para pasar al siguiente middleware en la cadena
         next();
